d6c849818be8a17dfe695b2ceefcb948b7daa4bd
Sam Badger <smalbadger@email.arizona.edu>
2017-12-16 12:46:25 -0700

    made a simple plot using plotly.

--- src/price_plotter.py
+++ src/price_plotter.py
@@ -1,4 +1,7 @@
 import gdax
+import plotly
+import plotly.plotly as py
+import plotly.graph_objs as go
 
 def main():
     Client = gdax.PublicClient()
@@ -27,4 +30,24 @@ def main():
     #task:
     #use the 2 lists above (times and averages) and make a nice lil plot using plotly.
     #NOTE: Before using plotly, you'll have to install their libraries. just go to your command line and type "pip install plotly" (without the quotes of course)
+
+    #I set up a plotly account for our team. Here's the info:
+    #email   : smalbadger@gmail.com
+    #Username: TradeBotTeam
+    #Password: wearetraders_bumbumbum
+    #API key : SJTSXTDYHHtwLyul8olP
+
+    #this updates the credentials file to access the API
+    plotly.tools.set_credentials_file(username='TradeBotTeam', api_key='SJTSXTDYHHtwLyul8olP')
+
+    # Create a trace
+    trace = go.Scatter(
+        x = times,
+        y = averages
+    )
+
+    data = [trace]
+    py.plot(data, filename="averages")
+
+    #Hi Nic, sorry I already made a graph because I was just really curious. See what else you can do with Plotly. I don't know if we'll be able to use the charts from plotly in a GUI, but we'll figure that out later. I think there is some way to save the charts locally instead of to the website. It's pretty simple to use. Let me know if you have any questions.
 main()
