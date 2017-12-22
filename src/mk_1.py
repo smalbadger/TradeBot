70fabc82652f9d53c08aeef22462e953bc9ed734
Sam Badger <smalbadger@email.arizonae.edu>
2017-12-22 10:55:39 -0700

    Got fake trading working temporarily. The code is a mess right now. Once GDAX is up and running again, I'll split the code into separate modules and clean everything up. Time to start making different versions.

--- src/mk_1.py
+++ src/mk_1.py
@@ -6,7 +6,6 @@ import plotly
 import plotly.plotly as py
 import plotly.graph_objs as go
 
-
 ################################################################################
 #                                 STATE                                        #
 ################################################################################
@@ -23,7 +22,7 @@ class state():
         self._high = 0
         self._upper_threshold = 0   #currently not using this
         self._lower_threshold = 0   #currently not using this
-        self._entry = None
+        self._entry = 0
 
         if "Sell" in name:
             self._transaction_type = "sell"
@@ -46,6 +45,13 @@ class state():
     def thresholds(self):
         return {"upper": self._upper_threshold, "lower":self._lower_threshold, "high": self._high, "low": self._low}
 
+
+    def set_high(self, new_high):
+        self._high = new_high
+		
+    def set_low(self, new_low):
+        self._low = new_low
+		
     def update_percent(self, new_percent):
         """
             This method updates the transaction percent for a state.
@@ -56,26 +62,40 @@ class state():
         """
            This method initiates a trade. Each state will make a trade, except for the hold state.
         """
-        if "buy" in self._name:
+        if "Buy" in self._name:
             side = "buy"
-        elif "sell" in self._name:
+        elif "Sell" in self._name:
             side = "sell"
         else:
             return
-        # product =
-        # price =
-        # order_type = "limit"
-        crypto, cash = bot.product_pool()
+            
+        available_cash = bot.cash()
+        available_crypto = bot.crypto()
+        price = bot.historical_prices()[-1]
+        
         if side == "buy":
-            size = (cash * (self._transaction_percent/100))/price
+            transaction_cash_value = available_cash * self.percent() / 100
+            fees = transaction_cash_value * .003 #assume that we'll always get charged the .3% buy fee.
+            transaction_crypto_size = transaction_cash_value / price
+            
+            bot._cash = bot.cash() - transaction_cash_value - fees
+            bot._crypto = bot.crypto() + transaction_crypto_size
+            
         elif side == "sell":
-            size = crypto * (self._transaction_percent/100)
+            transaction_crypto_size = available_crypto * self.percent() / 100
+            transaction_cash_value = price * transaction_crypto_size
+            
+            bot._cash = bot.cash() + transaction_cash_value
+            bot._crypto = bot.crypto() - transaction_crypto_size
+            
+        bot._price_at_trade.append(price)
+        bot._history_of_portfolio_value.append(bot.cash() + bot.crypto() * price)
 
 ################################################################################
 #                           FINITE STATE MACHINE                               #
 ################################################################################
 class FSM():
-    def __init__(self):
+    def __init__(self, transition_buffer):
         """
             This is the finite state machine that we'll be using to decide if we should buy or sell.
             It has 6 states, 5 of which will be frequently visited.
@@ -110,12 +130,14 @@ class FSM():
         SS._prev = CS
 
         H.update_percent(0)
-        SS.update_percent(10)
-        SB.update_percent(10)
+        WS.update_percent(15)
+        WB.update_percent(15)
+        SS.update_percent(30)
+        SB.update_percent(30)
         CS.update_percent(50)
 
         self._current_state = H
-        self._transition_buffer = .1
+        self._transition_buffer = transition_buffer
         self._trade_thread = None
         self._state_log = open("state_log.txt", "w")
 
@@ -128,7 +150,7 @@ class FSM():
             where the last element in the list is the newest. Contains logic to decide if we should move
             toward buy, toward sell, or stay at the same state.
         """
-        if len(history) == 0:
+        if len(history) == 0 or history[-1] == 0:
             return
 
         last_price = history[-1]
@@ -140,8 +162,12 @@ class FSM():
         low = thresh["low"]
 
         entry = cur_state.entry()
-        if entry == None:
-            self._entry = last_price
+        if entry == 0:
+            cur_state._entry = last_price
+            entry = last_price
+            
+        #For debugging purposes
+        #print("State: ", cur_state.name(), "\tentry: ", cur_state.entry(), "\tlast price: ", last_price)
 
         #update high and low
         if last_price > high:
@@ -151,15 +177,29 @@ class FSM():
             cur_state.set_low(last_price)
             low = last_price
 
+        next_state = cur_state
+
         #Decide if the bot should change states
         if cur_state.name() == "Strong-Buy":
             if last_price < (high - high * self._transition_buffer/100):
                 next_state = cur_state.prev()
 
-        elif cur_state.name() == "Weak-Buy" or cur_state.name() == "Hold" or cur_state.name() == "Weak-Sell":
-            if last_price < (entry - entry * self._transition_buffer/100):
+        elif cur_state.name() == "Weak-Buy":
+            if last_price < (entry - entry * (self._transition_buffer/100)):
+                next_state = cur_state.prev()
+            #elif last_price > (entry + entry * self._transition_buffer/100):
+            #    next_state = cur_state.next()
+        
+        elif cur_state.name() == "Hold":
+            if last_price < (entry - entry * (self._transition_buffer/100)):
                 next_state = cur_state.prev()
             elif last_price > (entry + entry * self._transition_buffer/100):
+                next_state = cur_state.next()
+        
+        elif cur_state.name() == "Weak-Sell":
+            #if last_price < (entry - entry * (self._transition_buffer/100)):
+            #    next_state = cur_state.prev()
+            if last_price > (entry + entry * self._transition_buffer/100):
                 next_state = cur_state.next()
 
         elif cur_state.name() == "Strong-Sell":
@@ -175,9 +215,17 @@ class FSM():
         else:
             print("ERROR: bot is at unknown state. Please check the logs.")
             sys.exit(1)
-
-        cur_state = next_state
-        cur_state._entry = last_price
+        
+        if last_price != 0 and cur_state.entry() != 0:
+            message = cur_state.name() + " -> " + next_state.name() + "\tentry price: " + str(cur_state.entry()) + "\tpercent change: " + str(round((last_price - cur_state.entry())/cur_state.entry(), 2)) + "%\n"
+            #self._state_log.write(message)
+            #print(message)
+            
+        if next_state != cur_state:
+            next_state._entry = last_price
+            
+        self._current_state = next_state
+        
 
     def run(self, robot):
         """
@@ -185,13 +233,12 @@ class FSM():
             we sleep for a short amount of time so we don't get blocked. (max server requests per second = 3)
         """
         def _trade_routine():
-            for i in range(60):
-                self._state_log.write(self.current_state().name())
+            while robot._running:
                 self.change_state(robot.historical_prices())
                 self._current_state.trade(robot)
-                #robot.create_portfolio()
                 time.sleep(.5)
             self._state_log.close()
+            #robot.create_portfolio()
 
         self._trade_thread = Thread(target=_trade_routine)
         self._trade_thread.start()
@@ -207,7 +254,7 @@ class BotSocket(gdax.WebsocketClient):
         self._message_count = 0
 
     def on_open(self):
-        print("-- Starting Bot Socket --")
+        #print("-- Starting Bot Socket --")
         self._history = []
         self._message_count = 0
         self.stop = 0
@@ -215,8 +262,8 @@ class BotSocket(gdax.WebsocketClient):
     def on_message(self, msg):
         self._message_count += 1
         if 'price' in msg and 'type' in msg:
-            print ("Message type:", msg["type"], "\t@ {:.3f}".format(float(msg["price"])))
             if msg["type"] == "done":   #a "done" message means that the it's being traded at that price.
+                #print ("Message type:", msg["type"], "\t@ {:.3f}".format(float(msg["price"])))
                 if len(self._history) >= self._history_size:
                     self._history = (self._history[1:]).append(float(msg["price"]))
                 else:
@@ -225,32 +272,37 @@ class BotSocket(gdax.WebsocketClient):
     def on_close(self):
         self.stop = 1
         self.thread.join()
-        print("-- Terminating Bot Socket --")
-        print("message count: " + str(self._message_count))
+        #print("-- Terminating Bot Socket --")
+        #print("message count: " + str(self._message_count))
 
 ################################################################################
 #                                   Bot                                        #
 ################################################################################
 class Bot():
-    def __init__(self, currency):
+    def __init__(self, name, currency, transition_buffer, webSocket):
         """
            This is the actual Bot. It contains account information including a client object needed to interact with GDAX.
         """
+        self._name = name
         self._passphrase = ""
         self._secret = ""
         self._key = ""
         self.get_credentials()
         self._client = gdax.AuthenticatedClient(self._key, self._secret, self._passphrase)
-        self._socket = BotSocket(product=currency, key=self._key, secret=self._secret, passphrase=self._passphrase)
-        self._fsm = FSM()
+        self._socket = webSocket
+        self._fsm = FSM(transition_buffer)
         self._currency = currency
         self._running = False
         self._crypto = 0
-        self._cash = 0
+        self._cash = 13400
+        self._price_at_trade = []
+        self._history_of_portfolio_value = []
 
         self.scramble_credentials()
 
     ##---- GETTERS ----##
+    def name(self):
+        return self._name
     def socket(self):
         return self._socket
     def client(self):
@@ -261,8 +313,10 @@ class Bot():
         return len(self._socket._history) == self._socket._history_size
     def historical_prices(self):
         return self._socket._history
-    def product_pool(self):
-        return self._crypto, self._cash
+    def cash(self):
+        return self._cash
+    def crypto(self):
+        return self._crypto
     def status(self):
         return self._running
     def get_balances(self):
@@ -285,6 +339,7 @@ class Bot():
         return USD, BTC, BCH, ETH, LTC
 
     def get_credentials(self):
+        """
         yay = 0
         while yay == 0:
             file_loc = input("Drag and drop credential file here, or type path: ")
@@ -294,7 +349,8 @@ class Bot():
             except:
                 print("Sorry, That file doesn't seem to exist. Please try again.")
                 yay = 0
-
+        """
+        cred_file = open("../credentials.txt")
         lines = cred_file.readlines()
         self._passphrase = lines[0].split()[1]
         self._key = lines[1].split()[1]
@@ -315,11 +371,14 @@ class Bot():
         ### This will spawn another thread (once I learn how that works) that continuously trades.
         ### The condition to run will look like "while self._running: (trade)"
         self._fsm.run(self)
+        print(self.name()+" has been started")
 
     def stop(self):
+        print(self.name()+" has been stopped")
         self._running = False   #shut down client
         self._fsm._trade_thread.join()     #close trading thread
         self.socket().close()   #shut down web socket.
+        
 
     def update_historic_prices(self, new):
         #This will change. I don't actually want to update the whole list,
@@ -374,12 +433,88 @@ class Bot():
             p_id = i["id"]
             if "USD" in p_id:
                 print(p_id + " : " + str(self.client().get_product_ticker(product_id=p_id)["price"]))
+                
+    def plot_session(self):
+        x_axis = []
+        for i in range(len(self._price_at_trade)):
+            x_axis.append(i)
+            
+        trace1 = go.Scatter(
+            x = x_axis,
+            y = self._price_at_trade,
+            name = 'crypto price',
+            connectgaps = True
+        )
+        trace2 = go.Scatter(
+            x = x_axis,
+            y = self._history_of_portfolio_value,
+            name = 'portfolio value',
+            connectgaps = True
+        )
+
+        data = [trace1, trace2]
+
+        fig = dict(data=data)
+        py.plot(fig, filename=(self.name()+"_results"))
 
 def main():
-    BitBot = Bot("BTC-USD")
-    BitBot.start()
-    time.sleep(60)      #makes the bot run for 1 minute. There must be a delay.
-    BitBot.stop()
-    BitBot.print_current_prices()
+    key = "beb4b062b68a47620d306f07c8256ca2"
+    secret = "IgMJoO0Xz8KHkP8wVduWVxen3BHgtnb2MjMU8mAop6VMh4/oD9oYhyizQS/fXrNIY+KclLOitutDymBZx8R+TQ=="
+    passphrase = "12qpfghl23jq" 
+    currency = "BTC-USD"
+    socket = BotSocket(product=currency, key=key, secret=secret, passphrase=passphrase)
+
+    BitBot1 = Bot("BitBot_1", "BTC-USD", .3, socket)
+    initial_value1 = BitBot1.cash()
+    
+    BitBot2 = Bot("BitBot_2", "BTC-USD", .4, socket)
+    initial_value2 = BitBot2.cash()
+    
+    BitBot3 = Bot("BitBot_3", "BTC-USD", .5, socket)
+    initial_value3 = BitBot3.cash()
+    
+    BitBot4 = Bot("BitBot_4", "BTC-USD", .6, socket)
+    initial_value4 = BitBot4.cash()
+    
+    BitBot1.start()
+    BitBot2.start()
+    BitBot3.start()
+    BitBot4.start()
+    
+    time.sleep(300)      #makes the bot run for 6 hours. There must be a delay.
+    
+    BitBot1.stop()
+    BitBot2.stop()
+    BitBot3.stop()
+    BitBot4.stop()
+    
+    final_value1 = BitBot1.cash() + BitBot1.crypto() * (BitBot1.historical_prices()[-1])
+    cash_gain1 = round(final_value1 - initial_value1, 2)
+    percent_gain1 = round((final_value1 - initial_value1) * 100 / initial_value1, 2)
+    print("BitBot1 made $" + str(cash_gain1) + " (" + str(percent_gain1) + "%)")
+    BitBot1.plot_session()
+    
+    final_value2 = BitBot2.cash() + BitBot2.crypto() * (BitBot2.historical_prices()[-1])
+    cash_gain2 = round(final_value2 - initial_value2, 2)
+    percent_gain2 = round((final_value2 - initial_value2) * 100 / initial_value2, 2)
+    print("BitBot2 made $" + str(cash_gain2) + " (" + str(percent_gain2) + "%)")
+    BitBot2.plot_session()
+    
+    final_value3 = BitBot3.cash() + BitBot3.crypto() * (BitBot3.historical_prices()[-1])
+    cash_gain3 = round(final_value3 - initial_value3, 2)
+    percent_gain3 = round((final_value3 - initial_value3) * 100 / initial_value3, 2)
+    print("BitBot3 made $" + str(cash_gain3) + " (" + str(percent_gain3) + "%)")
+    BitBot3.plot_session()
+    
+    final_value4 = BitBot4.cash() + BitBot4.crypto() * (BitBot4.historical_prices()[-1])
+    cash_gain4 = round(final_value4 - initial_value4, 2)
+    percent_gain4 = round((final_value4 - initial_value4) * 100 / initial_value4, 2)
+    print("BitBot4 made $" + str(cash_gain4) + " (" + str(percent_gain4) + "%)")
+    BitBot4.plot_session()
+    
+    print("Bitcoin gained " + str(round((BitBot1.historical_prices()[0] - BitBot1.historical_prices()[-1])*100/BitBot1.historical_prices()[0],2)) + "%")
+    
+    
+
 
 main()
